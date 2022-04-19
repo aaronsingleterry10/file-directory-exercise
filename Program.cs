0b6066b9db4bee00c9e78d3b4eb62a7d446c3f22
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-18 21:42:00 -0500

    completed exercise 1

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FilesExercise
 {
@@ -10,6 +11,20 @@ namespace FilesExercise
     {
         static void Main(string[] args)
         {
+            //1 - Write a program that reads a text 
+            //    file and displays the number of words.
+
+            var filePath = @"C:\Users\Aaron Singleterry\source\repos\FilesExercise\index.txt";
+            Console.WriteLine(NumberOfWordsFromFile(filePath));
+            //2 - Write a program that reads a text file 
+            //    and displays the longest word in the file.
+        }
+
+        public static int NumberOfWordsFromFile(string file)
+        {
+            var text = File.ReadAllText(file);
+            var textArray = text.Split(' ');
+            return textArray.Length;
         }
     }
 }
