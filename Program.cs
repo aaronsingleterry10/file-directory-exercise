d4d38744077864c865bf79734877a9b6fa6f5ec6
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-18 22:01:53 -0500

    completed exercise

--- Program.cs
+++ Program.cs
@@ -16,8 +16,29 @@ namespace FilesExercise
 
             var filePath = @"C:\Users\Aaron Singleterry\source\repos\FilesExercise\index.txt";
             Console.WriteLine(NumberOfWordsFromFile(filePath));
+
             //2 - Write a program that reads a text file 
             //    and displays the longest word in the file.
+
+            
+            Console.WriteLine(LongestWordFromFile(filePath));
+
+        }
+
+        public static string LongestWordFromFile(string file)
+        {
+            var textFile = File.ReadAllText(file);
+            var textArray = textFile.Split(' ');
+            string longestWord = textArray[0];
+            for (var i = 0; i < textArray.Length; i++)
+            {
+                if (longestWord.Length < textArray[i].Length)
+                {
+
+                    longestWord = textArray[i];
+                }
+            }
+            return longestWord;
         }
 
         public static int NumberOfWordsFromFile(string file)
